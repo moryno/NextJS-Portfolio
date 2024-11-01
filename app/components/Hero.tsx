c8617609c853f1eede5ec2a99c1b45696bb84506
moryno <mauricenganga41@gmail.com>
2024-11-01 17:35:44 +0300

    Add animation effect to the experience, project, contact

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,30 @@
 // import Image from "next/image"
+"use client"
 import { personalData } from "@/utils/data/personal-data"
-import Link from "next/link"
+// import Link from "next/link"
+import { motion } from "framer-motion";
+
+const conatinerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5
+    }
+  }
+}
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    }
+  }
+}
 
 const Hero = () => {
   return (
@@ -8,24 +32,44 @@ const Hero = () => {
         <div className="flex flex-wrap lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
-                    {/* <Image src={"/"} alt="Maurice Nganga" className="border border-stone-900 rounded-3xl" /> */}
+                    <motion.img 
+                      src={""} 
+                      alt="Maurice Nganga" 
+                      className="border border-stone-900 rounded-3xl"
+                      width={650}
+                      height={650}
+                      initial={{ x: 100, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{ duration: 1, delay: 1.5 }}
+                    />
                 </div>
             </div>
             <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start mt-10">
-                    <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">Maurice Nganga</h2>
-                    <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</span>
-                    <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+                <motion.div 
+                 initial="hidden"
+                 animate="visible"
+                 variants={conatinerVariants}
+                 className="flex flex-col items-center lg:items-start mt-10">
+                    <motion.h2 
+                     variants={childVariants}
+                     className="pb-2 text-4xl tracking-tighter lg:text-8xl">Maurice Nganga</motion.h2>
+                    <motion.span
+                     variants={childVariants}
+                     className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</motion.span>
+                    <motion.p 
+                     variants={childVariants}
+                     className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                         { personalData }
-                    </p>
-                    <Link href={""} 
+                    </motion.p>
+                    <motion.a href={""} 
+                      variants={childVariants}
                       target="_blank"
                       rel="noopener nonreferrer"
                       download
                       className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
                         Download Resume
-                      </Link>
-                </div>
+                      </motion.a>
+                </motion.div>
             </div>
         </div>
     </article>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { projectsData } from "@/utils/data/projects-data"
 import Image from "next/image"
+import { motion } from "framer-motion";
 
 interface Iproject{
     title: string,
@@ -10,18 +12,30 @@ interface Iproject{
 const Projects = () => {
   return (
     <article className='pb-4'>
-        <h2 className='my-20 text-center text-4xl'>Projects</h2>
+        <motion.h2 
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.5 }}
+          className='my-20 text-center text-4xl'>Projects</motion.h2>
         <div>
             {projectsData.map((project: Iproject, index: number) => (
                 <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                  <div className="w-full lg:w-1/4">
+                  <motion.div 
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className="w-full lg:w-1/4">
                     <Image src={""} 
                      width={250}
                      height={250}
                      alt={project.title}
                      className="mb-6 rounded" />
-                  </div>
-                  <div className="w-full max-w-xl lg:w-3/4">
+                  </motion.div>
+                  <motion.div 
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className="w-full max-w-xl lg:w-3/4">
                     <h3 className="mb-2 font-semibold text-2xl">{project.title}</h3>
                     <p className="mb-4 text-stone-400">{project.description}</p>
                     { project.technologies.map(( tech: string, index: number) => (
@@ -29,7 +43,7 @@ const Projects = () => {
                             { tech }
                         </span>
                     ))}
-                  </div>
+                  </motion.div>
                 </div>
             ))}
         </div>
