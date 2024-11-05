e56bd7dc199d0692492662bbea34bdf75583a7d0
moryno <mauricenganga41@gmail.com>
2024-11-05 19:35:05 +0300

    Update the layout of application

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 // import type { NextConfig } from "next";
 
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn-images-1.medium.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
