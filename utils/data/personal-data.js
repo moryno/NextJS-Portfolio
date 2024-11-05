b4f4b6876594746246bec90475698641c2e57e2b
moryno <mauricenganga41@gmail.com>
2024-11-05 19:31:30 +0300

    Add personal data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,11 +2,6 @@ export const personalData = `I am a passionate front-end developer with 3 years 
 
 I am dedicated to continuous learning and have earned certifications in Next.js and AWS Cloud Practitioner. As an active member of the Celo Foundation and various tech communities in Kenya, I strive to support diversity in tech. My proudest achievement includes being the 1st runner-up in a hackathon hosted by Ushahidi and the Celo Foundation. I am eager to collaborate on innovative projects that deliver exceptional user experiences and drive business growth.`;
 // export const personalData = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
-export const CONTACT = {
-  address: "Nairobi, Kenya ",
-  phoneNo: "+254 715 018 410 ",
-  email: "mauricenganga41@gmail.com",
-};
 export const PERSONALDATA = {
   name: "CREATIVE STUDIO",
   role: "Software Developer",
@@ -24,4 +19,6 @@ export const PERSONALDATA = {
   email: "mauricenganga41@gmail.com",
   linkedIn: "https://www.linkedin.com/in/mauricenganga/",
   gitHub: "https://github.com/moryno",
+  x: "https://twitter.com/MauriceNganga5",
+  mediumUsername: "mauricenganga41",
 };
